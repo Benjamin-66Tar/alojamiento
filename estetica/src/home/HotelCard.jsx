e6516192be0d9ea829530dab6318f3276b1f64a6
juane <juane@EDWLAN>
2026-05-26 16:08:54 -0600

    Corrige URLs de imagenes en produccion

--- estetica/src/home/HotelCard.jsx
+++ estetica/src/home/HotelCard.jsx
@@ -1,6 +1,6 @@
 import './HotelCard.css'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
+const API_URL = import.meta.env.VITE_API_URL ?? (import.meta.env.DEV ? 'http://localhost:3001' : '')
 
 export default function HotelCard({ hotel, onBook }) {
   const {
